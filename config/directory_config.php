a399e23753ef653f95eb772371490e25f435f1d2
Bayu Hafiz <bayu.hafiz@gmail.com>
2016-12-28 13:01:17 +0700

    updated: config dir

--- config/directory_config.php
+++ config/directory_config.php
@@ -8,8 +8,8 @@ $curr_urlpage = '';
 $arrayName = array('id','en');
 $arr_url = explode("/", $_SERVER['REQUEST_URI']);
 
-if(!isset($arr_url[1])) { redirect("klassique/id/index"); }
-if(! in_array($arr_url[1], array("id", "en")) )  { redirect("klassique/id/index"); }
+if(!isset($arr_url[1])) { redirect("/id/index"); }
+if(! in_array($arr_url[1], array("id", "en")) )  { redirect("/id/index"); }
 
 $curr_lang 		= $arr_url[1];
 $curr_page 		= $arr_url[2];
@@ -21,7 +21,7 @@ if(isset($arr_url[3])){
 
 $GLOBALS['curr_lang'] = $curr_lang;
 $GLOBALS['curr_page'] = $curr_page;
-$workdir = '/web/';
+$workdir = '/klassique/';
 
 if($curr_lang == 'id'){
 	$display_lang = "BAHASA INDONESIA";
