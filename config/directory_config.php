a80c7ad3fe849cef4a8dd1bdb1be6d59de6f5cb4
Bayu Hafiz <bayu.hafiz@gmail.com>
2016-12-28 12:59:56 +0700

    updated: config dir

--- config/directory_config.php
+++ config/directory_config.php
@@ -8,8 +8,8 @@ $curr_urlpage = '';
 $arrayName = array('id','en');
 $arr_url = explode("/", $_SERVER['REQUEST_URI']);
 
-if(!isset($arr_url[1])) { redirect("/id/index"); }
-if(! in_array($arr_url[1], array("id", "en")) )  { redirect("/id/index"); }
+if(!isset($arr_url[1])) { redirect("klassique/id/index"); }
+if(! in_array($arr_url[1], array("id", "en")) )  { redirect("klassique/id/index"); }
 
 $curr_lang 		= $arr_url[1];
 $curr_page 		= $arr_url[2];
